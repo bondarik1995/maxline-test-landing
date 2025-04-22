8d1b4820835237b22ee7276d9593c7874990fae0
Maria Lutsevich <m.a.bandaryk@gmail.com>
2025-04-22 19:05:01 +0300

    Fixes

--- src/components/Promo/index.js
+++ src/components/Promo/index.js
@@ -8,12 +8,10 @@ const Promo = () => {
   return (
     <div className={styles.container}>
       <picture className={styles.image}>
-        {/* <source media="(min-width:1024px)" srcSet={promoLg} /> */}
         <source media="(min-width:1440px)" srcSet={promoLg} />
         <img src={promo} alt="200 фриспинов" />
       </picture>
       <picture className={styles.imageSubtitle}>
-        {/* <source media="(min-width:1024px)" srcSet={promoSubtitleLg} /> */}
         <source media="(min-width:1440px)" srcSet={promoSubtitleLg} />
         <img src={promoSubtitle} alt="за регистрацию" />
       </picture>
